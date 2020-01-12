c92208b10d073bbfe44a8c3c0dff29a6082b87e3
Jenn <jenniferanne@live.com.au>
2020-01-12 19:55:27 +1100

    Update Home message

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,7 +5,7 @@ function Welcome() {
     return(
         <div className="container">
             <h3>Welcome to the Studio Ghibli API App.</h3>
-            <p>This is a React app to retrieve and display information about Studio Ghibli characters and films, using the <a className="link-inline" href="https://ghibliapi.herokuapp.com/">Studio Ghibli API</a>. The website uses resources from <a className="link-inline" href="https://www.toptal.com/designers/subtlepatterns/">subtlepatterns.com</a>, and API queries are based on information from </p>
+            <p>This is a React app to retrieve and display information about Studio Ghibli characters and films, using the <a className="link-inline" href="https://ghibliapi.herokuapp.com/">Studio Ghibli API</a>. The header uses a pattern from <a className="link-inline" href="https://www.toptal.com/designers/subtlepatterns/">subtlepatterns.com</a>, and the app was coded by Jennifer Ralph in January 2020.</p>
         </div>
     )
 }
